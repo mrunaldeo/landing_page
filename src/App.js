094e7e86315183494f1a8c1148d283f3fb69f5ad
Mrunal Deo <mrunaldeo16@gmail.com>
2024-08-10 18:51:08 +0530

    Add files via upload
    second commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./sections/Home";
+//import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+    
+    </div>
+  );
+};
+
+export default App;
